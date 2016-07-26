09e7985380a3671e3d55924fb797059dbc0a6670
Matthew Balch <matbalch@vt.edu>
2016-07-26 02:46:48 -0400

    Created basics for a loading/intro screen

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 A 3D browser based RTS in development at [hack.rva](http://www.hackrva.org/blog/)
 
+## Build
+
+In order to build this project install npm and run "npm install" in the base of the repository.
+
--- src/introscreen.js
+++ src/introscreen.js
@@ -1,13 +1,13 @@
 import { Screen } from 'canvas-screens';
-// import { Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, 
-//   WebGLRenderer } from 'three';
+// import { Scene, PerspectiveCamera, MeshBasicMaterial, Mesh, 
+//   WebGLRenderer, JSONLoader } from 'three';
 import THREE from 'three';
 const Scene = THREE.Scene;
 const PerspectiveCamera = THREE.PerspectiveCamera;
-const BoxGeometry = THREE.BoxGeometry;
 const MeshBasicMaterial = THREE.MeshBasicMaterial;
 const Mesh = THREE.Mesh;
 const WebGLRenderer = THREE.WebGLRenderer;
+const JSONLoader = THREE.JSONLoader;
 
 export const IntroScreen = Object.create(Screen);
 
@@ -17,23 +17,45 @@ IntroScreen.init = function(target, gl) {
 
   // Creates a perspective camera with a 1 meter near plane and 10,000 meter far plane
   // positioned at 0, 0, 1000
-  this.camera = new PerspectiveCamera( 75, target.width / target.height, 1, 10000);
-  this.camera.position.z = 1000;
+  this.camera = new PerspectiveCamera( 0.75, target.width / target.height, 1, 10000);
+  this.camera.position.z = 300;
 
   // Create a 200x200x200 meter red wireframe box
-  var geometry = new BoxGeometry(200, 200, 200);
+  this.renderer = new WebGLRenderer({
+    "canvas": target,
+    "context": gl
+  });
+
+  this.loader = new JSONLoader();
+  this.loader.load(
+    "resources/hackrva.json", 
+    this.onLoad.bind(this), 
+    this.onProgress.bind(this), 
+    this.onError.bind(this));
+
+  this.mesh = null;
+}
+
+IntroScreen.onLoad = function(geometry, materials) {
   var material = new MeshBasicMaterial( { 
     "color": 0xFF0000, 
     "wireframe": true} );
-  this.scene.add(new Mesh(geometry, material))
+  this.mesh = new Mesh(geometry, material);
+  this.scene.add(this.mesh);
+}
 
-  this.renderer = new WebGLRenderer({
-    "context": gl
-  });
+IntroScreen.onError = function() {
+  console.log("Failure while loading intro geometry");
+}
+
+IntroScreen.onProgress = function(xmlreq) {
 }
 
 IntroScreen.draw = function(currentTime) {
   Screen.draw.call(this, currentTime);
   this.renderer.render(this.scene, this.camera);
+  if(this.mesh !== null && this.elapsedTime < Math.PI / (2 * 0.05)) {
+    this.mesh.rotation.x = 0.05 * this.elapsedTime;
+  }
 }
 
