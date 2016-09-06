0004a8a8ebd839926e883d5710eb55667d06a0ab
Matthew Balch <matbalch@vt.edu>
2016-09-06 16:45:51 -0400

    Switched to TEXT type for SQL storage of offers and answers

--- scripts/init.sql
+++ scripts/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE offers (
   id INT NOT NULL AUTO_INCREMENT,
   room_id INT NOT NULL,
   used BOOLEAN DEFAULT FALSE,
-  contents VARCHAR(1000),
+  contents TEXT,
   CONSTRAINT pk_id PRIMARY KEY(id),
   CONSTRAINT fk_room_id FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);
 
@@ -16,7 +16,7 @@ CREATE TABLE answers (
   room_id INT NOT NULL,
   offer_id INT NOT NULL,
   used BOOLEAN DEFAULT FALSE,
-  contents VARCHAR(1000),
+  contents TEXT,
   CONSTRAINT pk_id PRIMARY KEY(id),
   CONSTRAINT fk_room_id FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE,
   CONSTRAINT fk_offer_id FOREIGN KEY(offer_id) REFERENCES offers(id) ON DELETE CASCADE);
